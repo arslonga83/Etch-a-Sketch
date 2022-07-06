0f33e78c3df633c79a50433347e0568a7cf1eadc
arslonga83 <jlwood83@gmail.com>
2022-07-06 16:36:41 -0700

    html boilerplate. javascript grid elements created. placeholder style to see squares while building.

---
+++ script.js
@@ -0,0 +1,14 @@
+//reference container where the divs will go
+const container = document.querySelector('.container');
+
+//create the given number of divs
+function createDivs (num) {
+    for (i=1; i<=num; i++) {
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('gridSquare');
+        container.appendChild(newDiv);
+    }
+}
+
+//call the function. eventually this will be paired with a prompt
+createDivs(16);
